fac1ed35ddd44e513fb21665d1542b522bdb10cd
elias <eliascamilo70@outlook.com>
2025-08-05 09:52:44 -0300

    Filter de livros por categoria

---
+++ app/metodoFilter.js
@@ -0,0 +1,8 @@
+const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
+
+btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront)
+
+function filtrarLivrosDeFront() {
+  let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
+  console.table(livrosFiltrados)
+}

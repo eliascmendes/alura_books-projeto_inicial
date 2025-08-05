b05e699e02cb53d51fbd05055f4d24e3dc99690f
elias <eliascamilo70@outlook.com>
2025-08-05 11:26:02 -0300

    refatoração de código

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -4,9 +4,10 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros) {
   elementoParaInserirLivros.innerHTML = ''
 listaDeLivros.forEach(livro => {
+  let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
   elementoParaInserirLivros.innerHTML += `
   <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
       <h2 class="livro__titulo">
         ${livro.titulo}
       </h2>
@@ -19,3 +20,11 @@ listaDeLivros.forEach(livro => {
   `
 })
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//   if (livro.quantidade > 0) {
+//     return 'livro__imagens'
+//   } else {
+//     return 'livro__imagens indisponivel'
+//   }
+// }
